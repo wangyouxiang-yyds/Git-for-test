7a2597fdadeb755b97c001ad26a676471380bb3a
wangyouxiang-yyds <tree8454@gmail.com>
2025-02-09 20:01:34 +0800

    write function1

---
+++ Project/function_1.py
@@ -0,0 +1,11 @@
+def function1(number):
+
+    num = int(input(number))
+
+    if num > 0:
+        print(f"{num} is greater than 0")
+    elif num < 0:
+        print(f"{num} is smaller than 0")
+    else:
+        print(f"{num} is eqaul 0")
+
